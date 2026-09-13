0c9638d40abe08c25c3dd762c0f10f1afec13256
Ted Senft <code@robotgryphon.com>
2026-09-13 03:44:10 -0400

    Delete the older tests and stop failing build

--- tests/Feather.GraphQL.Tests/StarWars/Client/Human.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Feather.GraphQL.Tests.StarWars.Client;
-
-public record Human
-{
-    [JsonPropertyName("name")]
-    public string? Name { get; init; }
-}
--- tests/Feather.GraphQL.Tests/StarWars/Client/HumanResponse.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-
-namespace Feather.GraphQL.Tests.StarWars.Client;
-
-public class HumanResponse
-{
-    [JsonPropertyName("human")]
-    public Human? Human { get; init; }
-}
